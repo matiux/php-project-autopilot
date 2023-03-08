080eafa76454eb5a846a2aa82bad3c727c2a3909
Matteo Galacci <m.galacci@gmail.com>
2023-03-08 16:16:37 +0100

    improves phpunit

--- src/project/functions/func_phpunit.sh
+++ src/project/functions/func_phpunit.sh
@@ -43,8 +43,6 @@ phpunit() {
     --configuration "$TOOLS_PATH"/phpunit/phpunit.xml.dist \
     --exclude-group learning \
     --colors=always \
-    --testdox \
-    --verbose \
     "$@"
 
   local TEST_STATUS=$?
